f44153a84a2146b22ae67539d773edb5105edb24
Elanngo-Madheswaran <madheswaranelanngo@gmail.com>
2024-06-03 20:30:09 +0530

    edited property list

--- property/src/components/property_list/property_list.js
+++ property/src/components/property_list/property_list.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import './property_list.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const PropertyList = () => {
-  return(
-    <div className='container m-5 text-center'>
-      
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    const fetchProperties = async () => {
+      try {
+        const res = await axios.get('https://ap-south-1.aws.data.mongodb-api.com/app/data-jqrtjox/endpoint/data/v1'); // Adjust the API endpoint
+        setProperties(res.data); // Assuming the response contains an array of properties
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchProperties();
+  }, []);
+
+  return (
+    <div className="property-list">
+      {properties.map((property) => (
+        <div key={property.id} className="property-box">
+          <h3>{property.name}</h3>
+          <p>Address: {property.address}</p>
+          <p>Phone Number: {property.phnnumber}</p>
+          <p>Description: {property.description}</p>
+          <p>Price: ${property.price}</p>
+        </div>
+      ))}
     </div>
   );
 };
